e28b1df445be66c068edebd3a91b1c444bd469ea
mikub <miroslav.kubicek@gmail.com>
2018-11-22 21:23:54 +0100

    upgrade to clojure 1.9.0

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://titanoboa.io"
   :license {:name "GNU Affero General Public License"
             :url "https://www.gnu.org/licenses/agpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [pl.joegreen/lambda-from-string "1.6"] ;;Note: if running on JDK use this instead: [pl.joegreen/lambda-from-string "1.6" :exclusions [org.eclipse.jdt.core.compiler/ecj]]
                  [com.stuartsierra/component "0.3.1"]
-                 [org.clojure/core.async "0.2.374"]
+                 [org.clojure/core.async "0.4.490"]
                  [org.clojure/tools.logging "0.3.1"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
                                                     javax.jms/jms
@@ -25,13 +25,13 @@
                  [ring-server "0.4.0"]
                  [ring "1.6.3"]
                  [ring/ring-defaults "0.1.5"]
-                 [ring-middleware-format "0.7.2" :exclusions [com.cognitect/transit-clj]]
+                 [ring-middleware-format "0.7.2" :exclusions [com.cognitect/transit-clj org.clojure/tools.reader]]
                  [com.cognitect/transit-clj "0.8.290"]
                  [com.taoensso/timbre "4.7.0"]
                  [compojure "1.5.2"]
                  [buddy "2.0.0"]
                  [clj-http "3.9.1"]
-                 [org.clojure/tools.reader "1.0.0"]
+                 [org.clojure/tools.reader "1.3.2"]
                  [clj-time "0.14.0"]
                  [org.clojure/java.jdbc "0.7.1"]
                  [com.mchange/c3p0 "0.9.5.2"]
