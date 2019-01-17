fcec65559d4151c12de79a65c0271c5ade354f97
mikub <miroslav.kubicek@gmail.com>
2019-01-17 08:45:35 +0100

    version bump for 0.7.3. release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.titanoboa/titanoboa "0.7.3-SNAPSHOT"
+(defproject io.titanoboa/titanoboa "0.7.3"
   :description "titanoboa.io is fully distributed, highly scalable and fault tolerant workflow orchestration platform"
   :url "http://titanoboa.io"
   :license {:name "GNU Affero General Public License"
