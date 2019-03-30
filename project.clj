b20ea946da7cc3e512e6ed59a52f7b83e8dfc706
mikub <miroslav.kubicek@gmail.com>
2019-03-30 10:40:32 +0100

    bump to v 0.8.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.titanoboa/titanoboa "0.7.5-SNAPSHOT"
+(defproject io.titanoboa/titanoboa "0.8.0"
   :description "titanoboa.io is fully distributed, highly scalable and fault tolerant workflow orchestration platform"
   :url "http://titanoboa.io"
   :license {:name "GNU Affero General Public License"
